84b53be6bc4531eb5a1a1b56253d8f7780493220
mjbryan <mjbyran10@gmail.com>
2020-01-31 12:18:31 +0100

    added reset button

--- resources/js/table.js
+++ resources/js/table.js
@@ -12,18 +12,23 @@ let inclusionList = [
 ];
 // ---
 // HELPER FUNCTIONS --- 
-String.prototype.capitalize = function() {
+String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
-//TESTING
-function removeElementbyValue(elementVal) {
-    // Removes an element from the document by value
-    //CAN ONLY BE UNIQUE VALUE
-    var element = document.querySelector(`*[value='${elementVal}']`);
-    element.parentNode.removeChild(element);
+//TABLE GENERATORS 
+function checkFilter() {
+    let filterPartyArr = [];
+    let filterState = 'All';
+    filterState = stateList.value;
+    for (const check of checkboxes) {
+        if (check.checked) {
+            filterPartyArr.push(check.value);
+        }
+    }
+    tableTag.innerHTML = '';
+    generateTableBody(tableTag, data.results[0].members, inclusionList, filterPartyArr, filterState);
+    generateTableHead(tableTag, data.results[0].members[0], inclusionList);
 }
-// ---
-
 function generateTableHead(table, data_set, inclusions) {
     //takes data keys, targets html table and populates a header
     let thead = table.createTHead();
@@ -49,14 +54,14 @@ function generateTableBody(table, data, inclusions, filters = [], stateFilter) {
     let states = []; //To be filled per iteration and then popped
     let possibleParties = []; //Array of parties of members displayed 
     for (let element of data) { //element == member
-        if((element.state === stateFilter) || (stateFilter === 'All')){
+        if ((element.state === stateFilter) || (stateFilter === 'All')) {
             possibleParties.push(element.party);
             //pop array with possible parties with current statefilter
         }
-        if ((filters.includes(element.party)) || (filters.length === 0)){
+        if ((filters.includes(element.party)) || (filters.length === 0)) {
             //checks when checkboxes and allows them to pop (if none then all)
             states.push(element.state); //builds states array
-            if((element.state === stateFilter) || (stateFilter === 'All')){
+            if ((element.state === stateFilter) || (stateFilter === 'All')) {
                 //checks selected state -> pops that, if 'all' then pops all
                 let row = table.insertRow();
                 let fullName = `${element.first_name} ${element.middle_name || ''} ${element.last_name}`
@@ -72,23 +77,23 @@ function generateTableBody(table, data, inclusions, filters = [], stateFilter) {
                             cell.innerHTML = text;
                         }
                     }
-                }  
+                }
             }
         }
     }
     disableCheck(possibleParties);
     popStateList(states);
 }
-
-function disableCheck(arr){ //disables the invalid parties with current filter
+//CHECKBOX FUNCTIONS
+function disableCheck(arr) { //disables the invalid parties with current filter
     const rCheck = document.getElementById('republican-check');
     const dCheck = document.getElementById('democrat-check');
     const iCheck = document.getElementById('independent-check');
     let checkboxes = document.querySelectorAll('input[type=checkbox]');
-    let parties = [];
     for (const checkbox of checkboxes) {
         checkbox.disabled = false;
     }
+    let parties = [];
     if (!(arr.includes('R'))) {
         rCheck.disabled = true;
         parties.push('Reublicans');
@@ -102,34 +107,52 @@ function disableCheck(arr){ //disables the invalid parties with current filter
     disabledMsg(parties);
 }
 
-function disabledMsg(parties){
+function disabledMsg(parties) {
     let checkDiv = document.querySelector('.filters-container');
     let newDiv = document.querySelector('.disable-msg');
-    if (newDiv){
+    if (newDiv) {
         newDiv.parentNode.removeChild(newDiv);
     }
     if (parties.length > 0) {
         let partyText = parties.join(' or ').toString();
         let htmlText = `
-        <div class="disable-msg alert alert-info" role="alert"">
-        Note: There are no members of the ${partyText} in the selected state.
+        <div class="disable-msg alert alert-info d-flex justify-content-between" role="alert"">
+            Note: There are no members of the ${partyText} in the selected state.
+            <input id="reset_state_btn" class="btn btn-primary" type="reset" value="Reset">
         </div>`
         checkDiv.insertAdjacentHTML('afterend', htmlText);
+        document.getElementById('reset_state_btn').addEventListener('click', resetTable)
     }
 }
-
+function resetTable(){
+    let stateList = document.querySelector('#state-option');
+    let opts = stateList.querySelectorAll('option');
+    let val = 'All';
+    for (let i = 0; i < opts.length; i++) {
+        const opt = opts[i];
+        if (opt.value == val) {
+            stateList.selectedIndex = i;
+            break;
+        }
+    }
+    let checkboxes = document.querySelectorAll('input[type=checkbox]');
+    for (const checkbox of checkboxes) {
+        checkbox.checked = false;
+    }
+    checkFilter();
+}
+//SELECT LIST FUNCTION
 function popStateList(arr) { //Prototype
     let stateList = document.querySelector('#state-option');
     arr = Array.from(new Set(arr)); //Removes duplicates
     arr.sort(); //Orgainises alphabetically
-    // let stateOptions = stateList.getElementsByTagName('option');
     let currentOpt = stateList.querySelector('option:checked');
     stateList.innerHTML = '';
-    
+
     arr.unshift('All');
     for (let i = 0; i < arr.length; i++) {
         const state = arr[i];
-        let htmlStr =  `<option value="${state}">${state}</option>`
+        let htmlStr = `<option value="${state}">${state}</option>`
         stateList.insertAdjacentHTML('beforeend', htmlStr);
         if (state == currentOpt.value) {
             stateList.selectedIndex = i;
@@ -137,26 +160,13 @@ function popStateList(arr) { //Prototype
     }
 }
 
-//Refactored and brought in from filters.js
+//Global Event Listners
 let stateList = document.querySelector('#state-option');
 stateList.addEventListener('change', checkFilter);
 let checkboxes = document.querySelectorAll('input[type=checkbox]');
 for (const checkbox of checkboxes) {
-        checkbox.addEventListener('change', checkFilter);
+    checkbox.addEventListener('change', checkFilter);
 }
-function checkFilter() {
-    let filterPartyArr = [];
-    let filterState = 'All';
-    filterState = stateList.value;
-    for (const check of checkboxes) {
-        if (check.checked) {
-            filterPartyArr.push(check.value);
-        }
-    }
-    tableTag.innerHTML = '';
-    generateTableBody(tableTag, data.results[0].members,inclusionList, filterPartyArr, filterState);
-    generateTableHead(tableTag, data.results[0].members[0], inclusionList);
-}
-window.onload = () => {
+window.onload = () => { //Runs on load to Gen table.
     checkFilter();
 };
